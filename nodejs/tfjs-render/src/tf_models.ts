98cc7f6b46d827f61466f0f360a3e5c43ffb7592
trinagle1289 <denny95090@gmail.com>
2024-03-21 01:52:09 +0800

    feat: 新增姿勢模型函式庫

---
+++ nodejs/tfjs-render/src/tf_models.ts
@@ -0,0 +1,79 @@
+import { PoseDetectorBuilder } from "./pose_models";
+
+import * as tfn from "@tensorflow/tfjs-node-gpu";
+import * as pose_detection from "@tensorflow-models/pose-detection";
+import { SupportedModels } from "@tensorflow-models/pose-detection";
+
+// 設定運算後端
+console.log(
+  (await tfn.setBackend("tensorflow"))
+    ? `Finish setting backend: ${tfn.getBackend()}`
+    : "Failed to set backend 'tensorflow'"
+);
+
+// 模型設定
+let blazeposeTfjsCfg: pose_detection.BlazePoseTfjsModelConfig = {
+  runtime: "tfjs",
+};
+let movenetMLCfg: pose_detection.MoveNetModelConfig = {
+  modelType: pose_detection.movenet.modelType.MULTIPOSE_LIGHTNING,
+};
+let movenetSLCfg: pose_detection.MoveNetModelConfig = {
+  modelType: pose_detection.movenet.modelType.SINGLEPOSE_LIGHTNING,
+};
+let movenetSTCfg: pose_detection.MoveNetModelConfig = {
+  modelType: pose_detection.movenet.modelType.SINGLEPOSE_THUNDER,
+};
+let posenetResNet50Cfg: pose_detection.PosenetModelConfig = {
+  architecture: "ResNet50",
+  outputStride: 16,
+  inputResolution: {
+    width: 257,
+    height: 257,
+  },
+};
+let posenetMobileNetV1Cfg: pose_detection.PosenetModelConfig = {
+  architecture: "MobileNetV1",
+  outputStride: 16,
+  inputResolution: {
+    width: 257,
+    height: 257,
+  },
+};
+
+// 模型物件
+var BlazeposeTfjs = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.BlazePose)
+  .withConfig(blazeposeTfjsCfg)
+  .build();
+var MovenetML = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.MoveNet)
+  .withConfig(movenetMLCfg)
+  .build();
+var MovenetSL = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.MoveNet)
+  .withConfig(movenetSLCfg)
+  .build();
+var MovenetST = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.MoveNet)
+  .withConfig(movenetSTCfg)
+  .build();
+var PosenetResNet50 = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.PoseNet)
+  .withConfig(posenetResNet50Cfg)
+  .build();
+var PosenetMobileNetV1 = await new PoseDetectorBuilder()
+  .withModel(SupportedModels.PoseNet)
+  .withConfig(posenetMobileNetV1Cfg)
+  .build();
+console.log("Finish Building Models");
+
+// 對外使用變數
+export {
+  BlazeposeTfjs,
+  MovenetML,
+  MovenetSL,
+  MovenetST,
+  PosenetResNet50,
+  PosenetMobileNetV1,
+};
