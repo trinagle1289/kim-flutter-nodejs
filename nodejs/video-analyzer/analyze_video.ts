2ae0098751ffe4ad94f7a373550bc35af555111a
郭科顯 <denny95090@gmail.com>
2024-03-21 20:31:44 +0800

    feat: 建立影片分析程式

---
+++ nodejs/video-analyzer/analyze_video.ts
@@ -0,0 +1,39 @@
+import { PoseRenderer } from "./src/img_renderer";
+import { MovenetST } from "./src/tf_models";
+import { TmpDir } from "./src/utils";
+
+import * as fs from "node:fs";
+
+import ffmpeg from "ffmpeg";
+import * as tfn from "@tensorflow/tfjs-node-gpu";
+
+// 影片路徑
+const VIDEO_DIR = "../../resources/video/20240321";
+const VIDEO_NAME = "A1-A5_1";
+const VIDEO_TYPE = ".mp4";
+const VIDEO_PATH = `${VIDEO_DIR}/${VIDEO_NAME}${VIDEO_TYPE}`;
+
+// 設定 tfjs 後端
+tfn.setBackend("tensorflow");
+
+// 清理並建立暫存資料夾
+let tmp = new TmpDir("./tmp");
+let tmp2 = new TmpDir("./tmp2");
+let tmp3 = new TmpDir("./tmp3");
+tmp.deleteAllFiles();
+tmp2.deleteAllFiles();
+tmp3.deleteAllFiles();
+
+// 建立所有影像幀
+let vidIn = await new ffmpeg(VIDEO_PATH);
+await vidIn
+  .setVideoSize("1080x1920", true, true)
+  .save(`${tmp.DirPath}/${VIDEO_NAME}(%05d).png`);
+
+// 儲存渲染後的所有圖片
+await tmp.handleAllFiles(async (_name, _path) => {
+  let img = tfn.node.decodePng(fs.readFileSync(_path));
+  let result = await MovenetST.estimatePoses(img);
+  let renderer = await new PoseRenderer().renderResult(_path, result);
+  await renderer.savePNG(`${tmp2.DirPath}/${_name}`);
+});
