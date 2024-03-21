d02cafd1dc3241f38793eab89480cb7cd95cd748
郭科顯 <denny95090@gmail.com>
2024-03-21 18:03:47 +0800

    style: 新增描述
    新增所有模型物件的描述

--- nodejs/video-analyzer/src/tf_models.ts
+++ nodejs/video-analyzer/src/tf_models.ts
@@ -52,26 +52,32 @@ let posenetMobileNetV1Cfg: pose_detection.PosenetModelConfig = {
 };
 
 // 模型物件
+/** BlazePose TFJS 版 */
 var BlazeposeTfjs = await new PoseDetectorBuilder()
   .withModel(SupportedModels.BlazePose)
   .withConfig(blazeposeTfjsCfg)
   .build();
+/** MoveNet Multipose Lightning */
 var MovenetML = await new PoseDetectorBuilder()
   .withModel(SupportedModels.MoveNet)
   .withConfig(movenetMLCfg)
   .build();
+/** MoveNet Single Pose Lightning */
 var MovenetSL = await new PoseDetectorBuilder()
   .withModel(SupportedModels.MoveNet)
   .withConfig(movenetSLCfg)
   .build();
+/** MoveNet Single Pose Thunder */
 var MovenetST = await new PoseDetectorBuilder()
   .withModel(SupportedModels.MoveNet)
   .withConfig(movenetSTCfg)
   .build();
+/** PoseNet ResNet50 */
 var PosenetResNet50 = await new PoseDetectorBuilder()
   .withModel(SupportedModels.PoseNet)
   .withConfig(posenetResNet50Cfg)
   .build();
+/** Posenet MobileNetV1 */
 var PosenetMobileNetV1 = await new PoseDetectorBuilder()
   .withModel(SupportedModels.PoseNet)
   .withConfig(posenetMobileNetV1Cfg)
