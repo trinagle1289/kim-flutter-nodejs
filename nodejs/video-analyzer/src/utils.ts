6c20db232be4bb031c2e9e062682c345716a93a7
trinagle1289 <denny95090@gmail.com>
2024-03-24 13:53:48 +0800

    fix: 調整並簡化 TmpDir 物件
    將此物件繼承 BaseDirectory，並且把重複的函式及其內容進行刪除

--- nodejs/video-analyzer/src/utils.ts
+++ nodejs/video-analyzer/src/utils.ts
@@ -5,6 +5,9 @@ import * as pathLib from "node:path";
 class BaseDirectory {
   /** 資料夾路徑 */
   protected _path: string;
+  /** 設定物件
+   * @param _path 資料夾路徑
+   */
   public constructor(_path: string) {
     this._path = _path;
   }
@@ -33,7 +36,9 @@ class BaseDirectory {
     }
   }
 
-  /** 刪除資料夾內容 */
+  /** 刪除資料夾內容
+   * @param showLog 顯示輸出資訊
+   */
   public deleteAllFiles(showLog: boolean = false): void {
     let files = fs.readdirSync(this._path);
     files.forEach((_file) => {
@@ -44,53 +49,27 @@ class BaseDirectory {
 }
 
 /** 暫存資料夾 (位於 tmp/) */
-export class TmpDir {
-  private path: string;
+export class TmpDir extends BaseDirectory {
   /** 建立暫存資料夾
-   * @param _path 建立路徑
+   * @param _path 資料夾路徑
    * @param toClean 是否清理資料夾
+   * @param showLog 是否顯示輸出資訊
    */
-  public constructor(_path: string = "./tmp", toClean: boolean = false) {
-    this.path = _path;
-    if (!fs.existsSync(this.path)) {
-      fs.mkdir(this.path, (_err) =>
-        console.log("Finish Creating TMP Directory.")
-      );
+  public constructor(
+    _path: string = "./tmp",
+    toClean: boolean = false,
+    showLog: boolean = false
+  ) {
+    super(_path);
+    if (!fs.existsSync(this._path)) {
+      fs.mkdirSync(this._path);
+      console.log("Finish Creating TMP Directory.");
     }
-    if (toClean) this.deleteAllFiles();
+    if (toClean) this.deleteAllFiles(showLog);
   }
-
-  /** 資料夾路徑 */
-  public get DirPath(): string {
-    return this.path;
-  }
-
-  /** 取得全部檔案名稱 */
-  public get AllFiles(): string[] {
-    return fs.readdirSync(this.path);
-  }
-
-  /** 處理全部檔案
-   * @param callback 對單個檔案的操作
-   */
-  public async handleAllFiles(
-    callback: (_file: string, _path: string, _idx: number) => Promise<void>
-  ): Promise<void> {
-    let _files = fs.readdirSync(this.path);
-    for (let _idx = 0; _idx < _files.length; _idx++) {
-      let _file = _files[_idx];
-      let _path = pathLib.join(this.path, _file);
-      await callback(_file, _path, _idx);
-    }
-  }
-
   /** 刪除資料夾內容 */
-  public deleteAllFiles(): void {
-    console.log("Deleting All TMP Files.");
-    let files = fs.readdirSync(this.path);
-    files.forEach((_file) => {
-      fs.unlinkSync(pathLib.join(this.path, _file));
-      console.log(`Delete ${_file}`);
-    });
+  public deleteAllFiles(showLog: boolean = false): void {
+    if (showLog) console.log("Deleting All TMP Files.");
+    super.deleteAllFiles(showLog);
   }
 }
