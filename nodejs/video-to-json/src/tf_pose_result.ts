9ae226b9f5ff96519d68615df636a55c0a173e9d
trinagle1289 <denny95090@gmail.com>
2024-04-02 00:47:46 +0800

    feat: 新增物件來運算和儲存姿勢偵測結果

--- nodejs/video-to-json/src/tf_pose_result.ts
+++ nodejs/video-to-json/src/tf_pose_result.ts
@@ -19,16 +19,60 @@ export async function getPngResult(
   return pose;
 }
 
-export async function getAllPngResult(pngPath: string): Promise<void> {
-  let img = tfn.node.decodePng(fs.readFileSync(pngPath));
-  img.dispose();
+/** FullPoseResult 建立器，用於建構 FullPoseResult 類別 */
+export class FullPoseResultBuilder {
+  // 圖片路徑
+  private _pngPath: string = "";
+  // 姿勢偵測結果
+  public BlazeposeTfjs: Pose[] = [];
+  public MovenetML: Pose[] = [];
+  public MovenetSL: Pose[] = [];
+  public MovenetST: Pose[] = [];
+  public PosenetMobileNetV1: Pose[] = [];
+  public PosenetResNet50: Pose[] = [];
+
+  /** 建構物件
+   * @param pngPath PNG 檔案路徑
+   */
+  constructor(pngPath: string) {
+    this._pngPath = pngPath;
+  }
+
+  /** 建構 FullPoseResult 物件
+   * @returns FullPoseResult 物件
+   */
+  public async build() {
+    let img = tfn.node.decodePng(fs.readFileSync(this._pngPath));
+
+    this.BlazeposeTfjs = await tf_models.BlazeposeTfjs.estimatePoses(img);
+    this.MovenetML = await tf_models.MovenetML.estimatePoses(img);
+    this.MovenetSL = await tf_models.MovenetSL.estimatePoses(img);
+    this.MovenetST = await tf_models.MovenetST.estimatePoses(img);
+    this.PosenetMobileNetV1 = await tf_models.PosenetMobileNetV1.estimatePoses(
+      img
+    );
+    this.PosenetResNet50 = await tf_models.PosenetResNet50.estimatePoses(img);
+
+    img.dispose();
+    return new FullPoseResult(this);
+  }
 }
 
+/** 全部姿勢的結果，會收錄全部模型偵測結果 */
 export class FullPoseResult {
-  private _blazeposeTfjs: Pose[];
-  private _movenetML: Pose[];
-  private _movenetSL: Pose[];
-  private _movenetST: Pose[];
-  private _posenetMobileNetV1: Pose[];
-  private _posenetResNet50: Pose[];
+  public BlazeposeTfjs: Pose[] = [];
+  public MovenetML: Pose[] = [];
+  public MovenetSL: Pose[] = [];
+  public MovenetST: Pose[] = [];
+  public PosenetMobileNetV1: Pose[] = [];
+  public PosenetResNet50: Pose[] = [];
+
+  constructor(builder: FullPoseResultBuilder) {
+    this.BlazeposeTfjs = builder.BlazeposeTfjs;
+    this.MovenetML = builder.MovenetML;
+    this.MovenetSL = builder.MovenetSL;
+    this.MovenetST = builder.MovenetST;
+    this.PosenetMobileNetV1 = builder.PosenetMobileNetV1;
+    this.PosenetResNet50 = builder.PosenetResNet50;
+  }
 }
