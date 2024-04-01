ef0135a80a3c5a5502ae6d82c294d9a0b473d6df
郭科顯 <denny95090@gmail.com>
2024-04-01 16:57:34 +0800

    feat: 新增 ffmpeg 工具
    新增將影片內容轉換成圖片的函式

---
+++ nodejs/video-to-json/src/ffmpeg_utils.ts
@@ -0,0 +1,21 @@
+import ffmpeg from "ffmpeg";
+
+/**
+ * 建立影片幀
+ * @param videoPath 影片路徑
+ * @param outputPath 輸出路徑
+ */
+export async function createVideoFrames(
+  videoPath: string,
+  outputPath: string,
+  videoSize: string = "1920x1080",
+  videoFrame: number = null
+): Promise<void> {
+  // 建立物件
+  let video = await new ffmpeg(videoPath);
+  // 設定影片
+  video.setVideoSize(videoSize, true, true);
+  if (videoFrame != null) video.setVideoFrameRate(videoFrame);
+  // 儲存影片
+  await video.save(outputPath);
+}
