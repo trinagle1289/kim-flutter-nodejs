86401e09edd396ed7ffb5a1f5b7eb4743f317fbf
trinagle1289 <denny95090@gmail.com>
2024-04-03 01:17:48 +0800

    fix: 大幅度修改單部影片的 json 輸出程式

--- nodejs/video-to-json/analyze_video_to_json.ts
+++ nodejs/video-to-json/analyze_video_to_json.ts
@@ -1,36 +1,61 @@
-import * as fs from "node:fs";
-import * as pth_lib from "node:path";
-import * as dirs from "./src/directory.js";
-import * as model from "./src/tf_models.js";
-import * as tfn from "@tensorflow/tfjs-node-gpu";
-import { Pose } from "@tensorflow-models/pose-detection";
-import ffmpeg from "ffmpeg";
-
 // 常數
-const VIDEO_PATH = "../../resources/video/20240321/A1-A5_1.mp4";
+const VIDEO_PATH = "../../resources/video/20240401/A1-A5_1.mp4";
 const VIDEO_SIZE = "1080x1920";
-const JSON_PATH = "./pose_result.json";
+const VIDEO_FRAME_RATE = 10;
+const JSON_PATH = "./tmp2/A1-A5_1";
 
-let tmp = new dirs.TmpDir("./tmp", true);
-let vid_name = pth_lib.parse(VIDEO_PATH).name;
-let poses: Pose[][] = [];
+import { TmpDir } from "./src/directory.js";
+import * as path_lib from "node:path";
+import { createVideoFrames } from "./src/ffmpeg_utils.js";
+import { Pose } from "@tensorflow-models/pose-detection";
+import { FullPoseResultBuilder } from "./src/tf_pose_result.js";
+import * as fs from "node:fs";
 
-// Create Video Frames
-let video = await new ffmpeg(VIDEO_PATH);
-await video
-  .setVideoSize(VIDEO_SIZE, true, true)
-  .setVideoFrameRate(1)
-  .save(`${tmp.DirPath}${vid_name}(%02d).png`);
+let tmp = new TmpDir("tmp", true, true);
+let tmp2 = new TmpDir(JSON_PATH, true, true);
+let videoPath = path_lib.parse(VIDEO_PATH);
 
-// Get Pose Result In Frames
-console.log("2");
-await tmp.handleAllFiles(async (p) => {
-  let img = tfn.node.decodePng(fs.readFileSync(p));
-  let pose = await model.BlazeposeTfjs.estimatePoses(img);
-  img.dispose();
-  //   console.log(`${pth_lib.parse(f).name}:\n`);
-  poses.push(pose);
+// 1. 建立影像幀
+await createVideoFrames(
+  VIDEO_PATH,
+  `${tmp.DirPath}${videoPath.name}(%03d).png`,
+  VIDEO_SIZE,
+  VIDEO_FRAME_RATE
+);
+
+// 2. 計算關鍵點並儲存成陣列
+let blazeposeTfjs: Pose[][] = [];
+let movenetML: Pose[][] = [];
+let movenetSL: Pose[][] = [];
+let movenetST: Pose[][] = [];
+let posenetMobileNetV1: Pose[][] = [];
+let posenetResNet50: Pose[][] = [];
+await tmp.handleAllFiles(async (path) => {
+  let builder = await new FullPoseResultBuilder(path).build();
+
+  blazeposeTfjs.push(builder.BlazeposeTfjs);
+  movenetML.push(builder.MovenetML);
+  movenetSL.push(builder.MovenetSL);
+  movenetST.push(builder.MovenetST);
+  posenetMobileNetV1.push(builder.PosenetMobileNetV1);
+  posenetResNet50.push(builder.PosenetResNet50);
 });
 
-fs.writeFileSync(JSON_PATH, JSON.stringify(poses));
-console.log("end");
+// 3. 將陣列物件儲存成 json 檔案
+fs.writeFileSync(
+  `${tmp2.DirPath}BlazeposeTfjs.json`,
+  JSON.stringify(blazeposeTfjs)
+);
+fs.writeFileSync(`${tmp2.DirPath}MovenetML.json`, JSON.stringify(movenetML));
+fs.writeFileSync(`${tmp2.DirPath}MovenetSL.json`, JSON.stringify(movenetSL));
+fs.writeFileSync(`${tmp2.DirPath}MovenetST.json`, JSON.stringify(movenetST));
+fs.writeFileSync(
+  `${tmp2.DirPath}PosenetMobileNetV1.json`,
+  JSON.stringify(posenetMobileNetV1)
+);
+fs.writeFileSync(
+  `${tmp2.DirPath}PosenetResNet50.json`,
+  JSON.stringify(posenetResNet50)
+);
+
+console.log("Finish saving json files.");
