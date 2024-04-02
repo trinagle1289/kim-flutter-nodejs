a6dbe96c9ba050baad2f709ee60f1f156d19c66a
trinagle1289 <denny95090@gmail.com>
2024-04-02 10:10:24 +0800

    feat: 新增影片解碼器設定

--- nodejs/video-to-json/src/ffmpeg_utils.ts
+++ nodejs/video-to-json/src/ffmpeg_utils.ts
@@ -9,13 +9,15 @@ export async function createVideoFrames(
   videoPath: string,
   outputPath: string,
   videoSize: string = "1920x1080",
-  videoFrame: number = null
+  videoFrame: number = null,
+  videoCodec: string = null
 ): Promise<void> {
   // 建立物件
   let video = await new ffmpeg(videoPath);
   // 設定影片
   video.setVideoSize(videoSize, true, true);
   if (videoFrame != null) video.setVideoFrameRate(videoFrame);
+  if (videoCodec != null) video.setVideoCodec(videoCodec);
   // 儲存影片
   await video.save(outputPath);
 }
