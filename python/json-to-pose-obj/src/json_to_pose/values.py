da388adf6a05584d6568911ae53a6d9ebda59ee6
郭科顯 <denny95090@gmail.com>
2024-04-23 19:22:21 +0800

    fix: 修正套件導入混亂問題

--- python/json-to-pose-obj/src/json_to_pose/values.py
+++ python/json-to-pose-obj/src/json_to_pose/values.py
@@ -4,16 +4,41 @@
 # In[ ]:
 
 
-if __name__ == "__main__":
-    import classes
-else:
-    from src.json_to_pose import classes
+import numpy as np
 
 
 # In[ ]:
 
 
-blazepose_lines = classes.LineConnections(
+class LineConnections:
+    """線條連接點"""
+
+    left_kpt: list[list[str]] = []  # 左邊關鍵點連接列表
+    right_kpt: list[list[str]] = []  # 右邊關鍵點連接列表
+    center_kpt: list[list[str]] = []  # 中間關鍵點連接列表
+    full_kpt: list[list[str]] = []  # 全關鍵點連接列表
+
+    def __init__(
+        self, left: list[list[str]], right: list[list[str]], center: list[list[str]]
+    ) -> None:
+        """初始化物件
+        Args:
+            left (list[list[str]]): 左邊連接列表
+            right (list[list[str]]): 右邊連接列表
+            center (list[list[str]]): 中間連接列表
+        """
+        self.left_kpt = left
+        self.right_kpt = right
+        self.center_kpt = center
+        lr_kpt = np.append(left, right, axis=0)
+        full_kpt = np.append(lr_kpt, center, axis=0)
+        self.full_kpt = full_kpt.tolist()
+
+
+# In[ ]:
+
+
+blazepose_lines = LineConnections(
     left=[
         # 頭部
         ["nose", "left_eye"],  # 0, 2
