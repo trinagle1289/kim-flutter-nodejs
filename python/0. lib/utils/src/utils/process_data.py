110aec127b2bafead6258f7c1302793a65745bcf
郭科顯 <denny95090@gmail.com>
2024-05-01 19:03:04 +0800

    feat: 完善關鍵點座標轉換函式

--- python/0. lib/utils/src/utils/process_data.py
+++ python/0. lib/utils/src/utils/process_data.py
@@ -21,15 +21,15 @@ def scaleYAxis(data: list, val: float) -> list:
         list: 縮放結果
     """
 
-    np_data = np.array(data)
-    layer_size = len(np_data.shape)
+    data_arr = np.array(data)
 
+    layer_size = len(data_arr.shape)
     if layer_size == 2:  # 二維陣列
-        np_data[:, 1] *= val
+        data_arr[:, 1] *= val
     elif layer_size == 3:  # 三維陣列
-        np_data[:, :, 1] *= val
+        data_arr[:, :, 1] *= val
 
-    return np_data.tolist()
+    return data_arr.tolist()
 
 
 # In[ ]:
@@ -45,37 +45,54 @@ def transYAxis(data: list, val: float) -> list:
     Returns:
         list: 平移結果
     """
+    data_arr = np.array(data)
 
-    np_data = np.array(data)
-    layer_size = len(np_data.shape)
-
+    layer_size = len(data_arr.shape)
     if layer_size == 2:  # 二維陣列
-        np_data[:, 1] += val
+        data_arr[:, 1] += val
     elif layer_size == 3:  # 三維陣列
-        np_data[:, :, 1] += val
+        data_arr[:, :, 1] += val
 
-    return np_data.tolist()
+    return data_arr.tolist()
 
 
 # In[ ]:
 
 
-def kpt_pos_to_plot_pos(kpt_pos: list) -> list:
-    """關鍵點座標轉換成圖表座標
+def translate_a_kpt_to_plot_pos(all_kpt_pos: list, origin_kpt_pos: list) -> list:
+    """轉換單個關鍵點座標轉換成圖表座標
 
     Args:
-        kpt_pos (list): 關鍵點座標
+        all_kpt_pos (list): 所有關鍵點座標(用於設定 y 軸平移量)
+        origin_kpt_pos (list): 原始關鍵點座標
 
     Returns:
         list: 圖表座標
     """
-    kpt = np.array(kpt_pos)
-    add_y = 0
+    all_kpts = np.array(all_kpt_pos)
+    add_y = all_kpts[:, 1].max()
 
-    if kpt.shape == 2:  # 二維陣列
-        add_y = kpt[:, 1].max()
-    elif kpt.shape == 3:  # 三維陣列
-        add_y = kpt[:, :, 1].max()
+    return transYAxis(scaleYAxis(origin_kpt_pos, -1), add_y)
 
-    return transYAxis(scaleYAxis(kpt, -1), add_y)
+
+# In[ ]:
+
+
+def translate_multi_kpt_to_plot_pos(
+    all_kpt_pos: list, *origin_kpt_args: list[list]
+) -> list:
+    """將多個關鍵點座標轉換成圖表座標
+
+    Args:
+        all_kpt_pos (list): 所有關鍵點座標(用於設定 y 軸平移量)
+        origin_kpt_args (list[list]): 多組原始關鍵點座標
+
+    Returns:
+        list: 圖表座標
+    """
+    kpt_pos_list = []
+    for origin_kpt_pos in origin_kpt_args:
+        kpt_pos_list.append(translate_a_kpt_to_plot_pos(all_kpt_pos, origin_kpt_pos))
+
+    return kpt_pos_list
 
