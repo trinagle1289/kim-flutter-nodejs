46d9c1f9ad4bf437cbb1b2d3d4bfabd04571e749
郭科顯 <denny95090@gmail.com>
2024-05-09 10:51:01 +0800

    feat: 新增將姿勢座標結果轉換成表格座標的函式

---
+++ python/pose-live-view/src/calculate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from mediapipe.tasks.python.vision.pose_landmarker import PoseLandmarkerResult
+
+from mediapipe_lib.base import PoseResult, ResultAnalyzer
+from utils.process_data import translate_multi_kpt_to_plot_pos
+
+
+# 將姿勢座標結果轉換成表格座標
+
+# In[ ]:
+
+
+def poselandmarker_result_to_plot_pos(
+    poselandmarker_result: PoseLandmarkerResult,
+    is_3d: bool = False,
+) -> list[list, list, list, list]:
+    """將姿勢座標結果轉換成表格座標
+
+    Args:
+        poselandmarker_result (PoseLandmarkerResult): 姿勢座標結果
+        is_3d (bool, optional): 是否為 3D 姿勢. Defaults to False.
+
+    Returns:
+        list[list, list, list, list]: [關鍵點, 左邊線條, 中間線條, 右邊線條]
+    """
+    # 建立分析器
+    result = PoseResult(poselandmarker_result)
+    analyzer = ResultAnalyzer(result)
+
+    # 取得關鍵點和線條資料
+    kpts = result.get_all_kpt_positions(is_3d)
+    left, center, right = analyzer.get_line_positions(get_3d=is_3d)
+
+    # 轉換關鍵點和線條資料
+    kpts, left, center, right = translate_multi_kpt_to_plot_pos(
+        kpts, kpts, left, center, right
+    )
+
+    return [kpts, left, center, right]
+
