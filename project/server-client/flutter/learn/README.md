5ac7bcf755b529980e44f9cfc4d7e571ea18e30a
trinagle1289 <denny95090@gmail.com>
2024-05-16 11:11:30 +0800

    feat: 先建立一個用於測試的程式碼

--- project/server-client/flutter/learn/README.md
+++ project/server-client/flutter/learn/README.md
@@ -1,3 +1,8 @@
 # learn
 
 A new Flutter project.
+
+## Packages
+
+* [http](https://pub.dev/packages/http)
+* [cronet_http](https://pub.dev/packages/cronet_http)
--- project/server-client/flutter/learn/lib/main.dart
+++ project/server-client/flutter/learn/lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
+import 'package:cronet_http/cronet_http.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +14,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton(
+              onPressed: () => startToTest(), child: const Text('test')),
         ),
       ),
     );
   }
+}
+
+/// 開始測試
+void startToTest() async {
+  final Client httpClient;
+  if (Platform.isAndroid) {
+    final engine = CronetEngine.build(
+        cacheMode: CacheMode.memory,
+        cacheMaxSize: 2 * 1024 * 1024,
+        userAgent: 'Book Agent');
+    httpClient = CronetClient.fromCronetEngine(engine, closeEngine: true);
+  } else {
+    httpClient = IOClient(HttpClient()..userAgent = 'Book Agent');
+  }
+
+  final response = await httpClient.get(Uri.https(
+      'www.googleapis.com',
+      'books/v1/volumes',
+      {'q': 'HTTP', 'maxResults': '40', 'printType': 'books'}));
+
+  httpClient.close();
 }
