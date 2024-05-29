1149473eda114cd3b8c0a8a853ee4c7964c3d0b1
trinagle1289 <denny95090@gmail.com>
2024-05-30 02:36:43 +0800

    feat: 更新伺服器主程式

--- projects/server-client/python/video-analyze-server/run-server.py
+++ projects/server-client/python/video-analyze-server/run-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_cors import cross_origin
 
 HOST = "127.0.0.1"
@@ -10,8 +10,20 @@ app = Flask(__name__)
 @app.route("/analyze/<video_id>", methods=["POST", "GET"])
 @cross_origin()
 def analyze_video(video_id: str):
-    data = f"Video id is: {video_id}"
-    response = app.response_class(response=data, status=200, content_type="text/plain")
+    content_type = "application/json"
+
+    response_data = {
+        "video id": video_id,
+        "start label": "",
+        "end label": "",
+        "extra 1": "",
+        "extra 2": "",
+        "extra 3": "",
+        "extra 4": "",
+    }
+
+    response = jsonify(response_data)
+
     return response
 
 
