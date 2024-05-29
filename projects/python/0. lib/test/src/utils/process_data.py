9014957b1e4db04ed77b60645b25bdcab526de0b
triangle1289 <44298650+trinagle1289@users.noreply.github.com>
2024-05-29 20:55:13 +0800

    feat: 新增對單點座標的轉換

--- projects/python/0. lib/test/src/utils/process_data.py
+++ projects/python/0. lib/test/src/utils/process_data.py
@@ -50,6 +50,8 @@ def transYAxis(data: list, val: float) -> list:
     data_arr = np.array(data)
 
     layer_size = len(data_arr.shape)
+    if layer_size == 1:  # 二維陣列
+        data_arr[1] += val
     if layer_size == 2:  # 二維陣列
         data_arr[:, 1] += val
     elif layer_size == 3:  # 三維陣列
