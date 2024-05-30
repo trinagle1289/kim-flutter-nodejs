7ab32de7f83a8a4b1457186d696d641de17cc301
triangle1289 <44298650+trinagle1289@users.noreply.github.com>
2024-05-30 17:48:24 +0800

    feat: 更新自定義程式碼以及新增影片的分析資訊

--- projects/python/0. lib/test/src/calculate.py
+++ projects/python/0. lib/test/src/calculate.py
@@ -14,10 +14,10 @@ from mediapipe.tasks.python.vision.pose_landmarker import PoseLandmarkerResult
 
 if __name__ == "__main__":
     from mediapipe_lib.base import PoseResult, ResultAnalyzer
-    from utils.process_data import translate_multi_kpt_to_plot_pos
+    from utils.positon_trans import translate_multi_kpt_to_plot_pos
 else:
     from src.mediapipe_lib.base import PoseResult, ResultAnalyzer
-    from src.utils.process_data import translate_multi_kpt_to_plot_pos
+    from src.utils.positon_trans import translate_multi_kpt_to_plot_pos
 
 
 # #### 函式
@@ -27,10 +27,10 @@ else:
 # In[ ]:
 
 
-def poselandmarker_result_to_plot_pos(
-    poselandmarker_result: PoseLandmarkerResult,
+def line_3d_result_to_plot_pos(
+    result: PoseLandmarkerResult,
 ) -> list[list, list, list, list]:
-    """將 3D 姿勢座標結果轉換成表格座標
+    """將 3D 姿勢座標的線條結果轉換成表格座標
 
     Args:
         poselandmarker_result (PoseLandmarkerResult): 姿勢座標結果
@@ -38,14 +38,15 @@ def poselandmarker_result_to_plot_pos(
     Returns:
         list[list, list, list, list]: [關鍵點, 左邊線條, 中間線條, 右邊線條]
     """
-    is_3d = True # 確定使用 3D 姿勢座標
+
+    is_3d = True  # 確定使用 3D 姿勢座標
 
     # 建立分析器
-    result = PoseResult(poselandmarker_result)
-    analyzer = ResultAnalyzer(result)
+    pose_result = PoseResult(result)
+    analyzer = ResultAnalyzer(pose_result)
 
     # 取得關鍵點和線條資料
-    kpts = result.get_all_kpt_positions(is_3d)
+    kpts = pose_result.get_all_kpt_positions(is_3d)
     left, center, right = analyzer.get_line_positions(get_3d=is_3d)
 
     # 轉換關鍵點和線條資料
@@ -54,4 +55,21 @@ def poselandmarker_result_to_plot_pos(
     )
 
     return [kpts, left, center, right]
+
+
+# ##### 取得在 bool 陣列中的 True 比率
+
+# In[ ]:
+
+
+def get_true_rate_in_bool_list(bool_lst: list[bool]) -> float:
+    """取得在 bool 陣列中的 True 比率
+
+    Args:
+        bool_lst (list[bool]): bool 陣列
+
+    Returns:
+        float: 在 bool 陣列中的 True 比率
+    """
+    return bool_lst.count(True) / len(bool_lst)
 
--- projects/python/0. lib/test/src/utils/plot_painter.py
+++ projects/python/0. lib/test/src/utils/plot_painter.py
@@ -3,7 +3,7 @@
 
 # ### 套件
 
-# In[1]:
+# In[ ]:
 
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg
@@ -121,57 +121,11 @@ def set_plot_labels(
     return ax
 
 
-# #### 繪製圖表
-
-# ##### 繪製姿勢結果折線圖
-
-# In[2]:
-
-
-def draw_pose_result_line_chart(
-    labels: list[str], data_type: str = None, ax: Axes = None
-) -> plt.Axes:
-    """繪製姿勢結果折線圖
-
-    Args:
-        labels (list[str]): 姿勢標籤列表
-        data_type (str, optional): 所代表的模型. Defaults to None.
-        ax (Axes, optional): 座標資料. Defaults to None.
-
-    Returns:
-        plt.Axes: 座標資料
-    """
-    # 取得當前使用的座標
-    if ax is None:
-        ax = plt.gca()
-
-    POSE_LABEL = ["A1", "A2", "A3", "A4", "A5"]
-
-    data = []
-    # 將標籤以數值的形式存入到 data 中
-    for lab in labels:
-        if lab == "A1":
-            data.append(0)
-        if lab == "A2":
-            data.append(1)
-        if lab == "A3":
-            data.append(2)
-        if lab == "A4":
-            data.append(3)
-        if lab == "A5":
-            data.append(4)
-
-    ax.set_xlabel("time frame")
-    ax.set_ylabel("pose label")
-    ax.set_yticks(list(range(5)), POSE_LABEL)
-    ax.plot(range(len(data)), data, label=data_type)
-
-    return ax
-
+# #### 基礎繪製
 
 # ##### 繪製多個點
 
-# In[3]:
+# In[ ]:
 
 
 def draw_dots(
@@ -215,7 +169,7 @@ def draw_dots(
 
 # ##### 繪製多組線條
 
-# In[4]:
+# In[ ]:
 
 
 def draw_lines(
@@ -265,9 +219,179 @@ def draw_lines(
     return ax
 
 
-# ### 資料轉換
+# #### 繪製資料
 
-# #### 將圖表轉換成圖片格式
+# ##### 繪製姿勢結果折線圖
+
+# In[ ]:
+
+
+def draw_pose_result_line_chart(
+    labels: list[str], data_type: str = None, ax: Axes = None
+) -> Axes:
+    """繪製姿勢結果折線圖
+
+    Args:
+        labels (list[str]): 姿勢標籤列表
+        data_type (str, optional): 所代表的模型. Defaults to None.
+        ax (Axes, optional): 座標資料. Defaults to None.
+
+    Returns:
+        plt.Axes: 座標資料
+    """
+    # 取得當前使用的座標
+    if ax is None:
+        ax = plt.gca()
+
+    # 姿勢標籤字典
+    POSE_LABEL_DICT = {"A1": 0, "A2": 1, "A3": 2, "A4": 3, "A5": 4}
+
+    data = []
+    # 將標籤以數值的形式存入到 data 中
+    for lab in labels:
+        data.append(POSE_LABEL_DICT[lab])
+
+    # 設定標籤名稱
+    ax.set_xlabel("time frame")
+    ax.set_ylabel("pose label")
+
+    # 設定 y 軸刻度
+    ticks = list(POSE_LABEL_DICT.values())
+    labels = list(POSE_LABEL_DICT.keys())
+    ax.set_yticks(ticks, labels)
+    ax.set_ylim(-0.1, 4.1)
+
+    # 繪製折線圖
+    x = range(len(data))
+    y = data
+    ax.plot(x, y, label=data_type)
+
+    return ax
+
+
+# ##### 繪製 bool 陣列折線圖
+
+# In[ ]:
+
+
+def draw_bool_list_line_chart(
+    bool_lst: list[bool], data_type: str = None, ax: Axes = None
+) -> Axes:
+    # 取得當前使用的座標
+    if ax is None:
+        ax = plt.gca()
+
+    data: list[int] = []
+    # 將 True 或 False 以數值的形式存入到 data 中
+    for element in bool_lst:
+        # True 資料訂為 1，False 資料訂為 0
+        if element:
+            data.append(1)
+        else:
+            data.append(0)
+
+    ax.set_xlabel("time frame")
+    ax.set_ylabel("do")
+    ax.set_yticks([1, 0], [True, False])  # 設定 y 軸刻度值
+    ax.set_ylim(-0.1, 1.1)
+    ax.plot(range(len(data)), data, label=data_type)
+
+    return ax
+
+
+# ##### 繪製頻率直方圖
+
+# In[ ]:
+
+
+def draw_frequency_list_bar_chart(
+    frequency_dict: dict[str:str],
+    ytick_label_dict: dict[str:int] = {
+        "FREQUENTLY_OR_CONSTANTLY": 2,
+        "OCCASIONALLY": 1,
+        "RARELY": 0,
+    },
+    ax: Axes = None,
+) -> Axes:
+    """繪製頻率直方圖
+
+    Args:
+        frequency_dict (dict[str:str]): 動作頻率字典{動作名稱: 頻率名稱}
+        ytick_label_dict (dict[str:int], optional): y 軸刻度值設定字典{頻率名稱: y 軸刻度值}. Defaults to { "FREQUENTLY_OR_CONSTANTLY": 2, "OCCASIONALLY": 1, "RARELY": 0, }.
+        ax (Axes, optional): 表格座標. Defaults to None.
+
+    Returns:
+        Axes: 表格座標
+    """
+
+    # 取得當前使用的座標
+    if ax is None:
+        ax = plt.gca()
+
+    # 表格資料
+    plot_data: dict[str, int] = {}
+    # 將頻率列表轉換成數值
+    for key, value in frequency_dict.items():
+        y_tick = ytick_label_dict[value]  # 設定 y 軸刻度值
+        print(y_tick)
+        plot_data.update({key: y_tick})  # 更新表格資料
+
+    # 設定標籤
+    ax.set_xlabel("Doing...")  # 做的事情
+    ax.set_ylabel("Frequency")  # 頻率
+
+    # 設定 y 軸刻度值
+    ticks = list(ytick_label_dict.values())
+    labels = list(ytick_label_dict.keys())
+    ax.set_yticks(ticks, labels)
+
+    # 繪製直方圖
+    x = list(plot_data.keys())
+    y = list(plot_data.values())
+    ax.bar(x, y, label="Frequency Type")
+
+    return ax
+
+
+# ##### 繪製動作執行比率直方圖
+
+# In[ ]:
+
+
+def draw_execution_rate_bar_chart(
+    execution_rate_dict: dict[str:int],
+    ax: Axes = None,
+) -> Axes:
+    """繪製動作執行比率直方圖
+
+    Args:
+        execution_rate_dict (dict[str:int]): 動作執行比率字典{動作名稱: 執行比率}
+        ax (Axes, optional): 圖表座標. Defaults to None.
+
+    Returns:
+        Axes: 圖表座標
+    """
+
+    # 取得當前使用的座標
+    if ax is None:
+        ax = plt.gca()
+
+    # 設定標籤
+    ax.set_xlabel("Doing...")  # 做的事情
+    ax.set_ylabel("Execution Rate")  # 執行比率
+    ax.set_ylim(0, 100)  # 顯示範圍為 0 ~ 100
+
+    # 繪製直方圖
+    x = list(execution_rate_dict.keys())
+    y = list(execution_rate_dict.values())
+    ax.bar(x, y, label="Execution Rate")
+
+    return ax
+
+
+# #### 資料轉換
+
+# ##### 將圖表轉換成圖片格式
 
 # In[ ]:
 
