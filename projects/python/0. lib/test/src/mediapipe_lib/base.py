fb388af520c91039bcca5ca89583035e9ee8aa7b
trinagle1289 <denny95090@gmail.com>
2024-06-02 02:16:26 +0800

    feat: 新增兩種方法來過濾多於標籤、完善取得開始與結束姿勢標籤的程式

--- projects/python/0. lib/test/src/mediapipe_lib/base.py
+++ projects/python/0. lib/test/src/mediapipe_lib/base.py
@@ -3,7 +3,7 @@
 
 # #### 套件
 
-# In[ ]:
+# In[1]:
 
 
 from enum import Enum
@@ -15,7 +15,7 @@ from typing import Self
 
 # ##### MediaPipe
 
-# In[ ]:
+# In[2]:
 
 
 import mediapipe as mp
@@ -36,7 +36,7 @@ from mediapipe.tasks.python.components.containers.landmark import (
 
 # ##### Custom
 
-# In[ ]:
+# In[3]:
 
 
 if __name__ == "__main__":
@@ -761,8 +761,79 @@ class LhcPoseListAnalyzer:
         return result
 
     def filter_label_list_in_fun_a(labels: list[str]) -> list[str]:
+        """透過方法 A 來過濾標籤列表
 
-        return
+        過濾方法:
+            每 30 幀抓姿勢，設定其 30 幀中的最多姿勢為其代表姿勢
+
+        Args:
+            labels (list[str]): 原始標籤列表
+
+        Returns:
+            list[str]: 過濾後的標籤列表
+        """
+        STEP = 30  # 每 STEP 幀抓姿勢
+
+        filtered_labels = []  # 過濾後的標籤列表
+
+        # i 為 [0, STEP, STEP*2, STEP*3 , ...]
+        for i in range(0, len(labels), STEP):
+            labels_in_step = labels[i : i + STEP]  # 在 STEP 中的所有標籤
+
+            # 拋棄最後不夠 STEP 幀影像的結果
+            if len(labels_in_step) < STEP:
+                break
+
+            # 添加 STEP 數量的最多姿勢標籤
+            filtered_labels += [max(labels_in_step)] * STEP
+
+        return filtered_labels
+
+    def filter_label_list_in_fun_b(labels: list[str]) -> list[str]:
+        """透過方法 B 來過濾標籤列表
+
+        過濾方法:
+            每 30 幀抓姿勢，觀察其 30 幀中的最多姿勢，並選擇其中有至少連續 5 幀的最多姿勢標籤為代表標籤
+
+        Args:
+            labels (list[str]): 原始標籤列表
+
+        Returns:
+            list[str]: 過濾後的標籤列表
+        """
+        STEP = 30  # 每 STEP 幀抓姿勢
+        MIN_CONSECUTIVE_TIMES = 5  # 姿勢標籤最小連續次數
+
+        filtered_labels = []  # 過濾後的標籤列表
+
+        # i 為 [0, STEP, STEP*2, STEP*3 , ...]
+        for i in range(0, len(labels), STEP):
+            labels_in_step = labels[i : i + STEP]  # 在 STEP 中的所有標籤
+
+            # 拋棄最後不夠 STEP 幀影像的姿勢標籤
+            if len(labels_in_step) < STEP:
+                break
+
+            # 取得標籤列表規則
+            rules = np.array(label_list_to_label_list_rule(labels_in_step))
+            # 取得標籤出現的全部數值
+            rules_num = rules[:, 1].astype(np.uint)
+            # 取得標籤出現次數的最大數值
+            max_num = rules_num.max()
+
+            # 當姿勢最大出現次數小於所需最小連續次數，將計入空字串到輸出資料中
+            if max_num < MIN_CONSECUTIVE_TIMES:
+                filtered_labels += [""] * STEP
+                continue
+
+            # 取得標籤出現次數為最大數值的索引值
+            max_idx = np.where(rules_num == max_num)
+            # 取得最大述職的姿勢標籤
+            max_label = rules[max_idx][0][0]
+            # 添加姿勢標籤結果
+            filtered_labels += [max_label] * STEP
+
+        return filtered_labels
 
     # LHC 資料列表
 
@@ -909,8 +980,22 @@ class LhcPoseListAnalyzer:
         Returns:
             list[str, str]: 姿勢標籤[開始, 結束]
         """
+        # 取得影片姿勢標籤
         labels = self.get_lhc_label_list(get_3d)
-        return ["", ""]
+
+        # 初始化開始、結束姿勢標籤
+        start, end = "", ""
+
+        # 取得已被過濾後的姿勢標籤列表
+        filtered = np.array(LhcPoseListAnalyzer.filter_label_list_in_fun_a(labels))
+        # 取得已被過濾和反轉後的姿勢標籤列表
+        reversed_filtered = np.array(filtered[::-1])
+
+        # 設定起始和結束姿勢標籤
+        start = filtered[0]
+        end = reversed_filtered[0]
+
+        return [start, end]
 
     def get_lhc_body_posture_rating_points(self, get_3d: bool = False) -> int:
         """取得 LHC 姿勢評級分數(不包含額外加分項)
