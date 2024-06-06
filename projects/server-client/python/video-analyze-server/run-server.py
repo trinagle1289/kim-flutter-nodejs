ba45c1dcb639fd6857262a0419ecf794ec19445a
triangle1289 <44298650+trinagle1289@users.noreply.github.com>
2024-06-06 18:39:27 +0800

    feat: 新增客戶端資料的處理功能

--- projects/server-client/python/video-analyze-server/run-server.py
+++ projects/server-client/python/video-analyze-server/run-server.py
@@ -1,22 +1,29 @@
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
+from pathlib import Path
+import numpy as np
+import cv2
 
-from analyze_video import get_video_json_result
+# custom
+from server_api import get_video_json_result
+
+# Datatype Reference
+from werkzeug.datastructures.file_storage import FileStorage
 
 HOST = "127.0.0.1"
 PORT = 8022
+SAVED_VIDEO_PATH = "video_in.mp4"
 
 app = Flask(__name__)
+saved_path = Path(SAVED_VIDEO_PATH)
 
 
 @app.route("/analyze/<video_id>", methods=["POST", "GET"])
 @cross_origin()
 def analyze_video(video_id: str):
 
-    file = request.files["file"]
-
-
-    print(type(file))
+    file: FileStorage = request.files["file"]
+    file.save("test.mp4")
 
     response_data = {
         "video id": video_id,
