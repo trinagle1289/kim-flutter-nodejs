43f5a923518ab0a3d3bee21f6b2f1f623d2d86c1
triangle1289 <44298650+trinagle1289@users.noreply.github.com>
2024-07-02 15:19:02 +0800

    feat: 新增取得多組點之間距離的函式

--- projects/python/0. lib/test/src/mediapipe_lib/calculator.py
+++ projects/python/0. lib/test/src/mediapipe_lib/calculator.py
@@ -3,7 +3,7 @@
 
 # #### 使用套件
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
@@ -48,56 +48,6 @@ def get_angle_by_3_points(pos1: list, pos2: list, center_pos: list) -> float:
     return angleA
 
 
-# ##### 將角度轉換成 LHC 姿勢標籤
-
-# In[ ]:
-
-
-def angles_to_lhc_label(angles: dict) -> str:
-    """將角度轉換成 LHC 姿勢標籤
-
-    標籤描述:
-        A1: 站立
-        A2: 搬運高處物品
-        A3: 微彎腰
-        A4: 彎腰
-        A5: 蹲姿、跪姿、跪坐姿勢
-
-    醫師判斷規則:
-        A1: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 0 - 90 度
-        A2: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 90 - 180 度
-        A3: 膝蓋 90 - 180 度，腰部 120 - 160 度
-        A4: 膝蓋 90 - 180 度，腰部 0 - 120 度
-        A5: 膝蓋 0 - 90 度
-
-    Args:
-        angles (dict): 角度字典
-
-    Returns:
-        str: LHC 姿勢標籤
-    """
-
-    label = ""  # 姿勢標籤
-
-    # 如果無法取得角度
-    if angles is {}:
-        label = ""
-    ### 左右邊關節只要觸發其中一種條件，就可以直接定義姿勢標籤了
-    elif angles["left_knee"] < 90 or angles["right_knee"] < 90:
-        label = "A5"
-    elif angles["left_hip"] < 120 or angles["right_hip"] < 120:
-        label = "A4"
-    # 角度區間之後會設定為 120-150 度，否則站立判斷過於嚴格
-    elif angles["left_hip"] < 160 or angles["right_hip"] < 160:
-        label = "A3"
-    elif angles["left_shoulder"] > 90 or angles["right_shoulder"] > 90:
-        label = "A2"
-    else:
-        label = "A1"
-
-    return label
-
-
 # ##### 取得兩條線座標之間的角度
 
 # In[ ]:
@@ -130,6 +80,33 @@ def get_angle_between_two_lines_position(
     return np.rad2deg(np.arccos(np.dot(vec1, vec2) / (norm1 * norm2)))
 
 
+# ##### 取得多組點之間的距離
+
+# In[7]:
+
+
+def get_dist_between_points(*point_list: list[list[float]]) -> float:
+    """取得多組點之間的距離
+
+    Args:
+        point_list (list[float]): 點列表
+
+    Returns:
+        float: 多組點之間的距離
+    """
+    dist = 0
+
+    if len(point_list) > 1:
+        pt1 = point_list[0]
+        for pt2 in point_list[1:]:
+            start_pt = np.array(pt1) # 起點
+            end_pt = np.array(pt2) # 終點
+            dist += np.linalg.norm(start_pt - end_pt) # 起點和終點距離
+            pt1 = end_pt.tolist()
+
+    return float(dist)
+
+
 # ##### 取得三角形重心座標
 
 # In[ ]:
@@ -155,6 +132,122 @@ def get_triangle_gravity_position(
     p3 = np.array(pos3)
 
     return ((p1 + p2 + p3) / 3).tolist()
+
+
+# ##### 將角度轉換成 LHC 姿勢標籤
+
+# In[ ]:
+
+
+def angles_to_lhc_label(angles: dict) -> str:
+    """將角度轉換成 LHC 姿勢標籤
+
+    標籤描述:
+        A1: 站立
+        A2: 搬運高處物品
+        A3: 微彎腰
+        A4: 彎腰
+        A5: 蹲姿、跪姿、跪坐姿勢
+
+    醫師判斷規則:
+        A1: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 0 - 90 度
+        A2: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 90 - 180 度
+        A3: 膝蓋 90 - 180 度，腰部 120 - 160 度
+        A4: 膝蓋 90 - 180 度，腰部 0 - 120 度
+        A5: 膝蓋 0 - 90 度
+
+    Args:
+        angles (dict): 角度字典
+
+    Returns:
+        str: LHC 姿勢標籤
+    """
+
+    label = ""  # 姿勢標籤
+
+    # 如果無法取得角度
+    if len(angles) <= 0:
+        label = ""
+    ### 左右邊關節只要觸發其中一種條件，就可以直接定義姿勢標籤了
+    elif angles["left_knee"] < 90 or angles["right_knee"] < 90:
+        label = "A5"
+    elif angles["left_hip"] < 120 or angles["right_hip"] < 120:
+        label = "A4"
+    # 角度區間之後會設定為 120-150 度，否則站立判斷過於嚴格
+    elif angles["left_hip"] < 160 or angles["right_hip"] < 160:
+        label = "A3"
+    elif angles["left_shoulder"] > 90 or angles["right_shoulder"] > 90:
+        label = "A2"
+    else:
+        label = "A1"
+
+    return label
+
+
+# ##### 將一邊的角度轉換成 LHC 姿勢標籤
+
+# In[ ]:
+
+
+def angles_to_lhc_label_by_one_side(angles: dict, is_left=False) -> str:
+    """將一邊的角度轉換成 LHC 姿勢標籤
+
+    標籤描述:
+        A1: 站立
+        A2: 搬運高處物品
+        A3: 微彎腰
+        A4: 彎腰
+        A5: 蹲姿、跪姿、跪坐姿勢
+
+    醫師判斷規則:
+        A1: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 0 - 90 度
+        A2: 膝蓋 90 - 180 度，腰部 160 - 180 度，肩膀 90 - 180 度
+        A3: 膝蓋 90 - 180 度，腰部 120 - 160 度
+        A4: 膝蓋 90 - 180 度，腰部 0 - 120 度
+        A5: 膝蓋 0 - 90 度
+
+    Args:
+        angles (dict): 角度字典
+        is_left (bool, optional): 是否為左側姿勢. Defaults to False.
+
+    Returns:
+        str: LHC 姿勢標籤
+    """
+
+    label = ""  # 姿勢標籤
+
+    if is_left:
+        # 如果無法取得角度
+        if len(angles) <= 0:
+            label = ""
+        elif angles["left_knee"] < 90:
+            label = "A5"
+        elif angles["left_hip"] < 120:
+            label = "A4"
+        # 角度區間之後會設定為 120-150 度，否則站立判斷過於嚴格
+        elif angles["left_hip"] < 160:
+            label = "A3"
+        elif angles["left_shoulder"] > 90:
+            label = "A2"
+        else:
+            label = "A1"
+    else:
+        # 如果無法取得角度
+        if len(angles) <= 0:
+            label = ""
+        elif angles["right_knee"] < 90:
+            label = "A5"
+        elif angles["right_hip"] < 120:
+            label = "A4"
+        # 角度區間之後會設定為 120-150 度，否則站立判斷過於嚴格
+        elif angles["right_hip"] < 160:
+            label = "A3"
+        elif angles["right_shoulder"] > 90:
+            label = "A2"
+        else:
+            label = "A1"
+
+    return label
 
 
 # ##### 將標籤列表轉換成標籤列表規則
