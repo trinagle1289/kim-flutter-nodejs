7146c46ae90f5bfa74ab6d5e85e276521c1bee53
triangle1289 <44298650+trinagle1289@users.noreply.github.com>
2024-07-08 17:46:54 +0800

    fix: 調整身體姿勢角度區間
    A3 的腰部角度為 115-145 度
    A4 的腰部角度為 0-115 度

--- projects/python/0. lib/test/src/mediapipe_lib/calculator.py
+++ projects/python/0. lib/test/src/mediapipe_lib/calculator.py
@@ -171,10 +171,11 @@ def angles_to_lhc_label(angles: dict) -> str:
     ### 左右邊關節只要觸發其中一種條件，就可以直接定義姿勢標籤了
     elif angles["left_knee"] < 80 or angles["right_knee"] < 80:
         label = "A5"
-    elif angles["left_hip"] < 130 or angles["right_hip"] < 130:
+    # 角度區間之後 A4 會設定臀部角度為 0-115 度
+    elif angles["left_hip"] < 115 or angles["right_hip"] < 115:
         label = "A4"
-    # 角度區間之後會設定為 120-150 度，否則站立判斷過於嚴格
-    elif angles["left_hip"] < 160 or angles["right_hip"] < 160:
+    # 角度區間之後 A3 會設定臀部角度為 115-150 度
+    elif angles["left_hip"] < 145 or angles["right_hip"] < 145:
         label = "A3"
     elif angles["left_shoulder"] > 90 or angles["right_shoulder"] > 90:
         label = "A2"
