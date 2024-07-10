72569bdb94a7882e31621e7316cdabba33d345fa
trinagle1289 <denny95090@gmail.com>
2024-07-10 21:52:24 +0800

    feat: 新增工作條件建議的開頭文字

--- projects/server-client/flutter/kim_lhc_app/lib/kim-lhc/part1.dart
+++ projects/server-client/flutter/kim_lhc_app/lib/kim-lhc/part1.dart
@@ -76,6 +76,8 @@ String resultPosture() {
 ///不良工作條件
 String conditionresult() {
   String condi = '';
+  condi = 'Working conditions:\n';
+  
   if (isListOneSelected && (selectedOneValue == 1 || selectedOneValue == 2)) {
     condi +=
         '•  Adjust your working posture to avoid movements that reach the limits of your joint range of motion.\n';
